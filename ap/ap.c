76d4c3b2fec517ee6cf955561d57c9eb2f32f38a
rcy <lyu2525@gmail.com>
2025-04-02 22:16:46 +0900

    feat: cdc 함수들 모듈화

--- ap/ap.c
+++ ap/ap.c
@@ -12,49 +12,20 @@
 void apInit(void)
 {
     uartOpen(_DEF_UART1, 57600);
-
 }
 
 void apMain(void)
 {
     uint32_t prev_time;
-    uint32_t prev_baud;
-    uint32_t led_blink_time;
 
-    if(resetGetCount() == 0)
-    {
-        led_blink_time = 100;
-    }
-    else
-    {
-        led_blink_time = 500;
-    }
-
-    prev_baud = uartGetBaud(_DEF_UART1);
     prev_time = millis();
 
 	while(1)
 	{
-	    if(millis() - prev_time >= led_blink_time)
+	    if(millis() - prev_time >= 500)
 	    {
 	        prev_time = millis();
 	        ledToggle(_DEF_LED1);
-
-	        uartPrintf(_DEF_UART1, "ResetCount : %d\n", resetGetCount());
 	    }
-
-	    if(uartAvailable(_DEF_UART1) > 0)
-	    {
-	        uint8_t rx_data;
-
-	        rx_data = uartRead(_DEF_UART1);
-	        uartPrintf(_DEF_UART1, "RxData : %c | 0x%X\n", rx_data, rx_data);
-	    }
-
-	    if(uartGetBaud(_DEF_UART1) != prev_baud)
-        {
-            prev_baud = uartGetBaud(_DEF_UART1);
-            uartPrintf(_DEF_UART1, "ChangedBaud : %d\n", uartGetBaud(_DEF_UART1));
-        }
 	}
 }
---
+++ hw/driver/cdc.c
@@ -0,0 +1,18 @@
+/*
+ * cdc.c
+ *
+ *  Created on: Apr 2, 2025
+ *      Author: fbcks
+ */
+
+
+#include "cdc.h"
+
+
+
+bool cdcInit(void)
+{
+    bool ret = true;
+
+    return ret;
+}
