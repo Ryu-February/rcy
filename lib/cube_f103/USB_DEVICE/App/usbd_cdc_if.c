e302c6ed1c478f4e8b0337c254c8664f9f21a24b
rcy <lyu2525@gmail.com>
2025-04-05 02:19:44 +0900

    feat: uart 통신 개선(못 했음)
    1. 파일 같은 거 usb에서 uart로 보낼 때
    -> 통신 속도 달라져 버퍼가 덮어씌워지는 현상

--- lib/cube_f103/USB_DEVICE/App/usbd_cdc_if.c
+++ lib/cube_f103/USB_DEVICE/App/usbd_cdc_if.c
@@ -32,10 +32,11 @@ USBD_CDC_LineCodingTypeDef LineCoding =
         };
 
 //Using Queue structure
-uint32_t rx_front = 0;
-uint32_t rx_rear = 0;
-uint32_t rx_size = 512;
-uint8_t rx_buf[512] = {0};
+uint32_t 	rx_front = 0;
+uint32_t 	rx_rear = 0;
+uint32_t 	rx_size = 512;
+uint8_t 	rx_buf[512] = {0};
+bool		rx_full = false;
 
 uint32_t cdcAvailable(void)
 {
@@ -102,6 +103,25 @@ uint32_t cdcWrite(uint8_t *p_data, uint32_t length)
 uint32_t cdcGetBaud(void)
 {
     return LineCoding.bitrate;
+}
+
+
+uint8_t USBD_CDC_SOF(struct _USBD_HandleTypeDef *pdev)
+{
+	if(rx_full == true)
+	{
+		uint32_t buf_len;
+			//수신 버퍼에 비어있는 데이터 양
+		buf_len = (rx_size - cdcAvailable()) - 1;
+
+		if(buf_len >= USB_FS_MAX_PACKET_SIZE)
+		{
+			//다음 데이터도 보내줘
+			USBD_CDC_ReceivePacket(pdev);
+			rx_full = false;
+		}
+	}
+	return 0;
 }
 /* USER CODE END INCLUDE */
 
@@ -350,18 +370,30 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   */
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
-  /* USER CODE BEGIN 6 */
-  USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
-  USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+	/* USER CODE BEGIN 6 */
+	for(int i = 0; i < *Len; i++)
+	{
+		cdcEnqueue(Buf[i]);
+	}
 
+	uint32_t buf_len;
+	//수신 버퍼에 비어있는 데이터 양
+	buf_len = (rx_size - cdcAvailable()) - 1;
 
-  for(int i = 0; i < *Len; i++)
-  {
-      cdcEnqueue(Buf[i]);
-  }
+	if(buf_len >= USB_FS_MAX_PACKET_SIZE)
+	{
+		//다음 데이터도 보내줘
+		USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
+		USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+	}
+	else
+	{
+		//버퍼 용량 부족하니 기다려라
+		rx_full = true;
+	}
 
-  return (USBD_OK);
-  /* USER CODE END 6 */
+	return (USBD_OK);
+	/* USER CODE END 6 */
 }
 
 /**
